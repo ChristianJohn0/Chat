1d5862ef0c778a148e04383d21a38a60da6fcd95
christian <christianjohn22@rocketmail.com>
2020-03-15 21:59:41 -0600

    Initial commit - Let's begin

---
+++ Chat/Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by Christian John on 2020-03-15.
+//  Copyright © 2020 Christian John. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
